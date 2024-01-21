8d7774c1caaebd4802a67ce443bc9a0009319168
mihirWeb <mihirpratapsingh166@gmail.com>
2024-01-21 14:32:55 +0530

    test to check the contribution from the gui and bash

--- test/RaffleTest2.t.sol
+++ test/RaffleTest2.t.sol
@@ -6,4 +6,5 @@ import {Test} from "forge-std/Test.sol";
 contract TestRaffle2 is Test{
     // this is the mf test to check the contributions from the terminal
     // this is the mf test to check the contributions from the gui
+    // this is the mf test to check the contributions from the bash and gui
 }
