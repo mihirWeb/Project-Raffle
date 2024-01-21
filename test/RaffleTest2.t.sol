106c490a9da2217414f91b095e2afbe10646e051
mihirWeb <mihirpratapsingh166@gmai.com>
2024-01-21 13:43:15 +0530

    test from gui

--- test/RaffleTest2.t.sol
+++ test/RaffleTest2.t.sol
@@ -4,5 +4,6 @@ pragma solidity ^0.8.18;
 import {Test} from "forge-std/Test.sol";
 
 contract TestRaffle2 is Test{
-    // this is the mf test to check the contributions
+    // this is the mf test to check the contributions from the terminal
+    // this is the mf test to check the contributions from the gui
 }
