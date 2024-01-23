098df645ad6a6f849ff2d72a619c399576194210
mihirWeb <mihirpratapsingh166@gmail.com>
2024-01-23 10:31:26 +0530

    revert tests are added to enterRaffle function

--- test/Unit/RaffleTest.t.sol
+++ test/Unit/RaffleTest.t.sol
@@ -3,15 +3,36 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {Raffle} from "../../src/Raffle.sol";
 
 contract RaffleUnitTest is Test{
 
     Raffle raffle;
+    HelperConfig helperConfig;
+
+    uint256 entranceFees; 
+    uint256 interval;
+    address vrfCoordinator; 
+    bytes32 gasLane;
+    uint64 subscriptionId;
+    uint32 callbackGasLimit;
+
+    event EnteredRaffle(address indexed player);
+
     address PLAYER = makeAddr("player");
 
     function setUp() external{
         DeployRaffle deployRaffle = new DeployRaffle();
+        helperConfig = new HelperConfig();
+            (
+                entranceFees, 
+                interval, 
+                vrfCoordinator, 
+                gasLane,
+                subscriptionId,
+                callbackGasLimit
+            ) = helperConfig.activeNetworkConfig();
         raffle = deployRaffle.run();
         vm.deal(PLAYER, 10 ether);
     }
@@ -26,6 +47,33 @@ contract RaffleUnitTest is Test{
         vm.expectRevert(Raffle.Raffle_NotEnoughEthSend.selector);
         raffle.enterRaffle();
 
+    }
+
+    function testEmitEventsOnEntry() public{
+        vm.prank(PLAYER);
+        vm.expectEmit(true, false, false, false); // out of 3 indexed values and unindexed value respectivle, i expect 1 indexed value will be emitted by this event/emit 
+
+        emit EnteredRaffle(PLAYER); // event that will emit
+
+        raffle.enterRaffle{value: entranceFees}(); // this will also emit an event then the test will check that both values are equal or not
+        
+    }
+
+    function testRevertWhenRaffleIsClosed() public{
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFees}(); // the player enters in the game
+        // calculation of winner will start after interval hence set block time:-
+        vm.warp(block.timestamp + interval + 1); // After every interval the block will start calculating hence so, this is calculating stage
+        vm.roll(block.number + 1); // this way the block will start from new block to make sure the time gap
+        // above conditions is to make sure it passes checkUpKeep function of raffle
+        
+        raffle.performUpkeep(""); // calculating
+
+        // now we will try to enter in Raffle
+
+        vm.prank(PLAYER);
+        vm.expectRevert(Raffle.Raffle_RaffleNotOpen.selector);
+        raffle.enterRaffle{value: entranceFees}();
     }
   
 }
